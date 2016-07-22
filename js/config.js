eddceff9150a380f52caf7cf5aeed35f4083f9e7
Fábio Rogério SJ <fabio.rogerio.sj@gmail.com>
2016-07-22 13:39:10 -0300

    template base

--- js/config.js
+++ js/config.js
@@ -1,21 +1,19 @@
 app.config(function($stateProvider, $urlRouterProvider) {
 
   $stateProvider
+
     .state('login', {
       url: "/login",
       templateUrl: "login.html",
       controller:"loginController"
     })
+
     .state('timeline', {
       url: "/timeline",
       templateUrl: "timeline.html",
       controller:"timelineController"
     })
 
-    if(localStorage.getItem("userID")){
-      $urlRouterProvider.otherwise('timeline');
-    } else {
-      $urlRouterProvider.otherwise('login');
-    }
+    $urlRouterProvider.otherwise('login');
 
 })
